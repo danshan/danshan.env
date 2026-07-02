deb2bc2f0fa8163f2273735ee38992e2fd6372af
Honghao SHAN 单弘昊 <honghao.shan@nio.com>
2026-07-02 17:22:27 +0800

    feature update for mise

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -18,10 +18,17 @@ eval $(/opt/homebrew/bin/brew shellenv)
 
 
 # ASDF https://asdf-vm.com/
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
+# if test -z $ASDF_DATA_DIR
+#     set _asdf_shims "$HOME/.asdf/shims"
+# else
+#     set _asdf_shims "$ASDF_DATA_DIR/shims"
+# end
+
+# mise https://mise.jdx.dev/ide-integration.html
+if status is-interactive
+  mise activate fish | source
 else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
+  mise activate fish --shims | source
 end
 
 # Do not use fish_add_path (added in Fish 3.2) because it
--- dotfiles/zsh/.zshrc
+++ dotfiles/zsh/.zshrc
@@ -173,3 +173,4 @@ eval "$(starship init zsh)"
 
 # claude 
 alias claude="claude --dangerously-skip-permissions"
+
